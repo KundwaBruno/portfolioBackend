4d79f7508d103f0e6449bb2cc6f2e70f68ab6c53
KundwaBruno <kundwabruno@gmail.com>
2020-10-21 04:32:18 +0200

    Fixed port issue

--- server.js
+++ server.js
@@ -52,4 +52,4 @@ app.use(express.static(path.join(__dirname, '/../public')));
 //     });
 
 let PORT = process.env.PORT;
-app.listen(PORT || 3000, console.log(`Server is running on port ${PORT}`));
+app.listen(PORT || 3000, console.log(`Server is running on port 3000`));
